8be54fed4cb61611f2efe14e906d17a398177026
mjini softwares <njauvictor.maina@gmail.com>
2026-07-16 20:09:26 +0300

    contact  commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,9 @@
+import ContactSection from "@/components/contact";
+
 export default function ContactPage() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Contact Us</h1>
-      <p className="text-lg">Contact page content will be added here.</p>
+    <div className="container mx-auto px-4 py-20">
+    <ContactSection/>
     </div>
   );
 }
